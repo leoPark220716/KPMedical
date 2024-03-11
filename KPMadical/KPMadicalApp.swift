3efce4d95c76261a2b1015d7c5fcb6ab99f5772f
leoPark220716 <109401877+leoPark220716@users.noreply.github.com>
2024-03-11 20:16:38 +0900

    Initial Commit

---
+++ KPMadical/KPMadicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPMadicalApp.swift
+//  KPMadical
+//
+//  Created by Junsung Park on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KPMadicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
